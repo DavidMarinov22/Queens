0fa7c4d677ce41f4975691d1781d8697d3768f6d
David Marinov <david12457@abv.bg>
2022-06-13 22:04:39 +0300

    Full game

---
+++ QueensGame/Board.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueensGame
+{
+    public class Board
+    {
+        private string[,] board;
+        private int sizeX = 0;
+        private int sizeY = 0;
+
+        public int SizeX 
+        {
+            get
+            {
+                return sizeX;
+            }
+            private set
+            {
+                if (value <= 3 || value > 30)
+                {
+                    throw new ArgumentException("!! - Your value for columns is invalid. - !!");
+                }
+                sizeX = value;
+            }
+        }
+        public int SizeY 
+        {
+            get
+            {
+                return sizeY;
+            }
+            private set
+            {
+                if (value <= 3 || value > 30)
+                {
+                    throw new ArgumentException("!! - Your value for rows is invalid. - !!");
+                }
+                sizeY = value;
+            }
+        }
+        
+        public Board(int sizeX, int sizeY)
+        {
+            this.SizeX = sizeX; 
+            this.SizeY = sizeY;
+
+            this.board = new string[SizeY, SizeX];
+            FillBoard();
+        }
+
+        public void DrawField(int top, int left)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                Console.SetCursorPosition(left, top + row);
+                for (int coll = 0; coll < board.GetLength(1); coll++)
+                {
+                    if (board[row, coll] == "{ * }")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(board[row, coll] + " ");
+                        Console.ResetColor();
+                    }
+                    else if (board[row, coll] == "{ 1 }" || board[row, coll] == "{ 2 }")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(board[row, coll] + " ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(board[row, coll] + " ");
+                        Console.ResetColor();
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public void InsertPlayer(string player, int playerRow, int playerCol)
+        {
+            string playerSymbol = "";
+            if (player == "Player1")
+                playerSymbol = "{ 1 }";
+            else
+                playerSymbol = "{ 2 }";
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int coll = 0; coll < board.GetLength(1); coll++)
+                {
+                    if (board[playerRow, playerCol] == "{ 1 }")
+                    {
+                        throw new ArgumentException("!! - You cannot place queen on this position. - !!"); 
+                    }
+                    else if (board[playerRow, playerCol] == "{ 2 }")
+                    {
+                        throw new ArgumentException("!! - You cannot place queen on this position. - !!");
+                    }
+                    else if(board[playerRow, playerCol] == "{ * }")
+                    {
+                        throw new ArgumentException("!! - You cannot place queen on this position. - !!");
+                    }
+                    else if (row == playerRow && coll == playerCol)
+                    {
+                        int[] startColAndRow = GetStartColAndRow(playerRow, playerCol);
+                        board[row, coll] = playerSymbol;
+                        FillMoves(startColAndRow, playerRow, playerCol);
+                        return;
+                    }
+                }
+            }
+            
+        }
+        public bool CheckBordIsFull()
+        {
+            foreach (var item in board)
+            {
+                if (item == "{ 0 }")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void FillMoves(int[] startColAndRow, int playerRow, int playerCol)
+        {
+            int row = startColAndRow[1];
+            int col = startColAndRow[0];
+            //DiagonalLeftToRight
+            for (int r = row; r < board.GetLength(0); r++)
+            {
+                for (int coll = col; coll < board.GetLength(1); coll++)
+                {
+                    if (board[r, coll] == "{ 1 }" || board[r, coll] == "{ 2 }")
+                    {
+                        break;
+                    }
+                    board[r, coll] = "{ * }";
+                    break;
+                }
+                col++;
+            }
+            //LeftAndRight
+            for (int c = 0; c < board.GetLength(1); c++)
+            {
+                if (board[playerRow, c] == "{ 1 }")
+                {
+                    continue;
+                }
+                else if (board[playerRow, c] == "{ 2 }")
+                {
+                    continue;
+                }
+                else
+                {
+                    board[playerRow, c] = "{ * }";
+                }
+            }
+            //TopAndBottom
+            for (int r = 0; r < board.GetLength(0); r++)
+            {
+                if (board[r, playerCol] == "{ 1 }")
+                {
+                    continue;
+                }
+                else if (board[r, playerCol] == "{ 2 }")
+                {
+                    continue;
+                }
+                else
+                {
+                    board[r, playerCol] = "{ * }";
+                }
+            }
+            //DiagonalRightToLeft
+            int[] rights = GetStartColAndRowRight(playerRow, playerCol);
+            row = rights[1];
+            col = rights[0];
+            for (int r = row; r < board.GetLength(0); r++)
+            {
+                for (int coll = col; coll >= 0; coll++)
+                {
+                    if (board[r, coll] == "{ 1 }" || board[r, coll] == "{ 2 }")
+                    {
+                        break;
+                    }
+                    board[r, coll] = "{ * }";
+                    break;
+                }
+                col--;
+            }
+        }
+        private int[] GetStartColAndRow(int playerRow, int playerCol)
+        {
+            int[] startColAndRow = new int[2];
+            startColAndRow[0] = playerCol;
+            startColAndRow[1] = playerRow;
+            if (playerCol == 0)
+            {
+                return startColAndRow;
+            }
+            if (startColAndRow[1] == 0)
+            {
+                return startColAndRow;
+            }
+            if (startColAndRow[0] >= startColAndRow[1])
+            {
+                for (int row = startColAndRow[1] - 1; row >= 0; row--)
+                {
+                    startColAndRow[0]--;
+                    startColAndRow[1]--;
+                }
+            }
+            else
+            {
+                for (int col = startColAndRow[0] - 1; col >= 0; col--)
+                {
+                    startColAndRow[0]--;
+                    startColAndRow[1]--;
+                }
+            }
+
+            return startColAndRow;
+        }
+        private int[] GetStartColAndRowRight(int playerRow, int playerCol)
+        {
+            int[] startColAndRow = new int[2];
+            startColAndRow[0] = playerCol;
+            startColAndRow[1] = playerRow;
+            if (playerCol == board.GetLength(1) - 1)
+            {
+                return startColAndRow;
+            }
+            else if (playerRow == board.GetLength(0) - 1)
+            {
+                for (int col = playerCol; col < board.GetLength(1) - 1; col++)
+                {
+                    startColAndRow[1]--;
+                    startColAndRow[0]++;
+
+                }
+                return startColAndRow;
+            }
+            else if (playerCol == 0)
+            {
+                for (int col = playerCol; col < board.GetLength(1) - 1; col++)
+                {
+                    if (startColAndRow[1] == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        startColAndRow[1]--;
+                        startColAndRow[0]++;
+                    }
+
+                }
+            }
+            if (startColAndRow[0] >= startColAndRow[1])
+            {
+                for (int col = startColAndRow[0] + 1; col < board.GetLength(1); col++)
+                {
+                    if (startColAndRow[1] == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        startColAndRow[0]++;
+                        startColAndRow[1]--;
+                    }
+                }
+            }
+            else
+            {
+                for (int col = startColAndRow[0] + 1; col < board.GetLength(1); col++)
+                {
+                    if (startColAndRow[1] == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        startColAndRow[0]++;
+                        startColAndRow[1]--;
+                    }
+                }
+            }
+
+            return startColAndRow;
+        }
+        private void FillBoard()
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int coll = 0; coll < board.GetLength(1); coll++)
+                {
+                    board[row, coll] = "{ 0 }";
+                }
+            }
+        }
+    }
+}
---
+++ QueensGame/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Text;
+
+namespace QueensGame
+{
+    internal class Program
+    {
+        /*
+            *   ________                           ________  ___________    ________                                      
+               /  _____/_____    _____   ____      \_____  \ \_   _____/    \_____  \  __ __   ____   ____   ____   ______
+              /   \  ___\__  \  /     \_/ __ \      /   |   \ |    __)       /  / \  \|  |  \_/ __ \ /    \ /    \ /  ___/
+              \    \_\  \/ __ \|  Y Y  \  ___/     /    |    \|     \       /   \_/.  \  |  /\  ___/|   |  \   |  \\___ \ 
+               \______  (____  /__|_|  /\___  >    \_______  /\___  /       \_____\ \_/____/  \___  >___|  /___|  /____  >
+                      \/     \/      \/     \/             \/     \/               \__>           \/     \/     \/     \/ 
+        */
+        static void Main()
+        {
+            //Pre declared variables
+            Console.Title = "Queens";
+            Board board = null;
+
+            bool check = false;
+            bool boardFull = false;
+            bool sizeDone = false;
+
+            int sizeOfX = 0, sizeOfY = 0;
+
+            string player = "Player1";
+            string continueGame = "";
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            //Menu
+            DrawTitle();
+            Console.WriteLine("1.Play");
+            Console.WriteLine("2.Help");
+            Console.WriteLine("3.Exit");
+            Console.WriteLine();
+            bool isTrue = false;
+            while (!isTrue)
+            {
+                Console.Write("Choose: ");
+                string options = Console.ReadLine();
+                switch (options)
+                {
+                    case "1":
+                        Console.Clear();
+                        DrawTitle();
+                        isTrue = true;
+                        break;
+                    case "2":
+                        Console.WriteLine();
+                        DrawHelp();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!! - Please enter valid option. - !!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+            
+            //Size of the board
+            while (!sizeDone)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the board sizes:");
+                    Console.Write(" The amount of columns(X) - ");
+                    check = int.TryParse(Console.ReadLine(), out sizeOfX);
+                    if(!check)
+                    {
+                        throw new ArgumentException("!! - Your value for columns is invalid. - !!");
+                    }
+                    Console.Write(" The amount of rows(Y) - ");
+                    check = int.TryParse(Console.ReadLine(), out sizeOfY);
+                    if (!check)
+                    {
+                        throw new ArgumentException("!! - Your value for rows is invalid. - !!");
+                    }
+                    board = new Board(sizeOfX, sizeOfY);
+                    sizeDone = true;
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message.ToString());
+                    Console.ResetColor();
+                    sizeDone = false;
+                }
+            }
+
+            //Draw board 
+            Console.Clear();
+            DrawTitle();
+            // --DrawBorder
+            string[][] border = new string[sizeOfY + 1][];
+            DrawBorder(border, sizeOfY, sizeOfX);
+
+
+            // --Positioning field next to border
+            int row = Console.GetCursorPosition().Top - sizeOfY;
+            int col = Console.GetCursorPosition().Left + 2;
+            board.DrawField(row, col);
+
+           
+
+            //Controller
+            while (!boardFull)
+            {
+                try
+                {
+                    if (player == "Player1")
+                    {
+                        Console.WriteLine("Player 1 enter the coordinates you want: ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 2 enter the coordinates you want: ");
+                    }
+
+                    //Value x
+                    Console.Write("The value for (x) - ");
+                    playerCol = int.Parse(Console.ReadLine()) - 1;
+                    if (playerCol < 0 || playerCol >= board.SizeX)
+                    {
+                        throw new ArgumentException("!! - The value for (x) is invalid. - !!");
+                    }
+
+                    //Value Y
+                    Console.Write("The value for (y) - ");
+                    playerRow = int.Parse(Console.ReadLine()) - 1;
+                    if (playerRow < 0 || playerRow >= board.SizeX)
+                    {
+                        throw new ArgumentException("!! - The value for (y) is invalid. - !!");
+                    }
+                    board.InsertPlayer(player, playerRow, playerCol);
+                    
+                    //Redraw field
+                    Console.Clear();
+                    DrawTitle();
+                    DrawBorder(border, sizeOfY, sizeOfX);
+                    row = Console.GetCursorPosition().Top - sizeOfY;
+                    col = Console.GetCursorPosition().Left + 2;
+                    board.DrawField(row, col);
+                    if (player == "Player1")
+                        player = "Player2";
+                    else
+                        player = "Player1";
+                    boardFull = board.CheckBordIsFull();
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message.ToString());
+                    Console.ResetColor();
+                }
+            }
+            //End and Winner
+            Console.Clear();
+            DrawWinner(player);
+            Console.Write("Do you want to play more ? \n(Y / N): ");
+            continueGame = Console.ReadLine();
+            continueGame = continueGame.ToLower();
+            if (continueGame == "y")
+            {
+                Console.Clear();
+                Main();
+            }
+        }
+
+        private static void DrawHelp()
+        {
+            Console.WriteLine("ABOUT THE GAME");
+            Console.WriteLine(" --Queens game is a board game played between two players.\n    The game is the work of the great minds of the I Can Here and Now initiative, in 2022 in June.\n    The game includes strategic thinking and good concentration into the game. ");
+            Console.WriteLine("GAMEPLAY");
+            Console.WriteLine(" --At the beginning you need to set the size of the board, which should be in the range of 4 - 30.\n    Then the first player must set the coordinates for the queen he wants to place in the range of 1 - n size.\n    The '*' are possible moves for queen and player cannot place another queen there.\n   The winner is the one who last puts a queen and fills the whole board.");
+        }
+
+        private static void DrawWinner(string player)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (player == "Player1")
+            {
+                sb.AppendLine("__________.__                                 ________              .__         ._.");
+                sb.AppendLine("\\______   \\  | _____  ___.__. ___________     \\_____  \\     __  _  _|__| ____   | |");
+                sb.AppendLine(" |     ___/  | \\__  \\<   |  |/ __ \\_  __ \\     /  ____/     \\ \\/ \\/ /  |/    \\  | |");
+                sb.AppendLine(" |    |   |  |__/ __ \\___   \\ ___/|  | \\/     /       \\      \\     /|  |   |  \\  \\|");
+                sb.AppendLine(" |____|   |____(____  / ____|\\___  >__|       \\_______ \\      \\/\\_/ |__|___|  /  __");
+                sb.AppendLine("       \\/\\/         \\/                                \\/       \\/   \\/");
+
+            }
+            else
+            {
+                sb.AppendLine("__________.__                                 ____             .__          ._.");
+                sb.AppendLine("\\______   \\  | _____  ___.__. ___________    /_   |    __  _  _|__| ____    | |");
+                sb.AppendLine(" |     ___/  | \\__  \\<   |  |/ __ \\_  __ \\    |   |    \\ \\/ \\/ /  |/    \\   | |");
+                sb.AppendLine(" |    |   |  |__/ __ \\___   \\  ___/|  | \\/    |   |     \\     /|  |   |  \\   \\|");
+                sb.AppendLine(" |____|   |____(____  / ____|\\___  >__|       |___|      \\/\\_/ |__|___|  /   __");
+                sb.AppendLine("       \\/\\/         \\/                                    \\/   \\/");
+            }
+            sb.AppendLine("════════════════════════════════════════════════════════════════════════════════════");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(sb.ToString());
+            Console.ResetColor();
+        }
+        private static void DrawTitle()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("═════════════════════════════════════════════════════════════════════════════════════════════════════════════");
+            sb.AppendLine("  ________                           ________  ___________    ________");
+            sb.AppendLine(" /  _____/_____    _____   ____      \\_____  \\ \\_   _____/    \\_____  \\  __ __   ____   ____   ____   ______");
+            sb.AppendLine("/   \\  ___\\__  \\  /     \\_/ __ \\      /   |   \\ |    __)       /  / \\  \\|  |  \\_/ __ \\ /    \\ /    \\ /  ___/");
+            sb.AppendLine("\\    \\_\\  \\/ __ \\|  Y Y  \\  ___/     /    |    \\|     \\       /   \\_/.  \\  |  /\\  ___/|   |  \\   |  \\___ \\ ");
+            sb.AppendLine(" \\______  (____  /__|_|  /\\___  >    \\_______  /\\___  /       \\_____\\ \\_/____/  \\___  >___|  /___|  /____  >");
+            sb.AppendLine("        \\/     \\/      \\/     \\/             \\/     \\/               \\__>           \\/     \\/     \\/     \\/ ");
+            sb.AppendLine("═════════════════════════════════════════════════════════════════════════════════════════════════════════════");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(sb.ToString());
+            Console.ResetColor();
+        }
+
+        private static void DrawBorder(string[][] border, int sizeOfY, int sizeOfX)
+        {
+            for (int y = 0; y < sizeOfY + 1; y++)
+            {
+                if (y == 0)
+                {
+                    border[y] = new string[sizeOfX + 1];
+                    for (int x = 0; x < sizeOfX + 1; x++)
+                    {
+                        if (x == 1)
+                        {
+                            border[y][x] = "   " + (x);
+                        }
+                        else if (x != 1 && x > 0)
+                        {
+                            border[y][x] = "    " + (x);
+                        }
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(string.Join(" ", border[y]));
+                }
+                else
+                {
+                    border[y] = new string[1];
+                    border[y][0] = (y).ToString();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(string.Join(" ", border[y]));
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
